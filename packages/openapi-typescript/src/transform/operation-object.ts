68cdbbfd0eaf8f3b4de12b0a95681cd5fa37732d
zhennann <zhen.nann@icloud.com>
2025-10-04 20:29:37 +0800

    feat: update

--- packages/openapi-typescript/src/transform/operation-object.ts
+++ packages/openapi-typescript/src/transform/operation-object.ts
@@ -1,7 +1,19 @@
 import ts from "typescript";
-import { addJSDocComment, NEVER, oapiRef, QUESTION_TOKEN, tsModifiers, tsPropertyIndex } from "../lib/ts.js";
+import {
+  addJSDocComment,
+  NEVER,
+  TRUE,
+  oapiRef,
+  QUESTION_TOKEN,
+  tsModifiers,
+  tsPropertyIndex,
+} from "../lib/ts.js";
 import { createRef } from "../lib/utils.js";
-import type { OperationObject, RequestBodyObject, TransformNodeOptions } from "../types.js";
+import type {
+  OperationObject,
+  RequestBodyObject,
+  TransformNodeOptions,
+} from "../types.js";
 import { transformParametersArray } from "./parameters-array.js";
 import transformRequestBodyObject from "./request-body-object.js";
 import transformResponsesObject from "./responses-object.js";
@@ -12,12 +24,14 @@ import transformResponsesObject from "./responses-object.js";
  */
 export default function transformOperationObject(
   operationObject: OperationObject,
-  options: TransformNodeOptions,
+  options: TransformNodeOptions
 ): ts.TypeElement[] {
   const type: ts.TypeElement[] = [];
 
   // parameters
-  type.push(...transformParametersArray(operationObject.parameters ?? [], options));
+  type.push(
+    ...transformParametersArray(operationObject.parameters ?? [], options)
+  );
 
   // requestBody
   if (operationObject.requestBody) {
@@ -30,14 +44,16 @@ export default function transformOperationObject(
           });
     const required = !!(
       "$ref" in operationObject.requestBody
-        ? options.ctx.resolve<RequestBodyObject>(operationObject.requestBody.$ref)
+        ? options.ctx.resolve<RequestBodyObject>(
+            operationObject.requestBody.$ref
+          )
         : operationObject.requestBody
     )?.required;
     const property = ts.factory.createPropertySignature(
       /* modifiers     */ tsModifiers({ readonly: options.ctx.immutable }),
       /* name          */ tsPropertyIndex("requestBody"),
       /* questionToken */ required ? undefined : QUESTION_TOKEN,
-      /* type          */ requestBodyType,
+      /* type          */ requestBodyType
     );
     addJSDocComment(operationObject.requestBody, property);
     type.push(property);
@@ -47,8 +63,8 @@ export default function transformOperationObject(
         /* modifiers     */ tsModifiers({ readonly: options.ctx.immutable }),
         /* name          */ tsPropertyIndex("requestBody"),
         /* questionToken */ QUESTION_TOKEN,
-        /* type          */ NEVER,
-      ),
+        /* type          */ NEVER
+      )
     );
   }
 
@@ -58,9 +74,33 @@ export default function transformOperationObject(
       /* modifiers     */ tsModifiers({ readonly: options.ctx.immutable }),
       /* name          */ tsPropertyIndex("responses"),
       /* questionToken */ undefined,
-      /* type          */ transformResponsesObject(operationObject.responses ?? {}, options),
-    ),
+      /* type          */ transformResponsesObject(
+        operationObject.responses ?? {},
+        options
+      )
+    )
   );
+
+  // security
+  if (operationObject.security) {
+    const securityItem = operationObject.security.find(
+      (item) => !!(item as any).bearerAuth
+    );
+    if (securityItem) {
+      type.push(
+        ts.factory.createPropertySignature(
+          /* modifiers     */
+          tsModifiers({ readonly: options.ctx.immutable }),
+          /* name          */
+          tsPropertyIndex("authToken"),
+          /* questionToken */
+          void 0,
+          /* type          */
+          TRUE
+        )
+      );
+    }
+  }
 
   return type;
 }
@@ -69,11 +109,13 @@ export default function transformOperationObject(
 export function injectOperationObject(
   operationId: string,
   operationObject: OperationObject,
-  options: TransformNodeOptions,
+  options: TransformNodeOptions
 ): void {
   // find or create top-level operations interface
   let operations = options.ctx.injectFooter.find(
-    (node) => ts.isInterfaceDeclaration(node) && (node as ts.InterfaceDeclaration).name.text === "operations",
+    (node) =>
+      ts.isInterfaceDeclaration(node) &&
+      (node as ts.InterfaceDeclaration).name.text === "operations"
   ) as unknown as ts.InterfaceDeclaration;
   if (!operations) {
     operations = ts.factory.createInterfaceDeclaration(
@@ -84,7 +126,7 @@ export function injectOperationObject(
       /* name            */ ts.factory.createIdentifier("operations"),
       /* typeParameters  */ undefined,
       /* heritageClauses */ undefined,
-      /* members         */ [],
+      /* members         */ []
     );
     options.ctx.injectFooter.push(operations);
   }
@@ -98,7 +140,7 @@ export function injectOperationObject(
       /* modifiers     */ tsModifiers({ readonly: options.ctx.immutable }),
       /* name          */ tsPropertyIndex(operationId),
       /* questionToken */ undefined,
-      /* type          */ ts.factory.createTypeLiteralNode(type),
+      /* type          */ ts.factory.createTypeLiteralNode(type)
     ),
   ]);
 }
